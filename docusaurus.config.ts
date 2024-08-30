ad5e17fdf0a88cdcd0020d79f1dec38c61aee678
HieuLe <lthieu9601@gmail.com>
2024-08-30 16:55:44 +0700

    feat: add landing page

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,100 +3,112 @@ import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: "BenKon Product Support",
-  tagline:
-    "Thanks for purchasing BenKon solutions. Here you will find product FAQs, product user manuals and installation videos for your device.",
-  favicon: "img/favicon.png",
+	title: "BenKon Product Support",
+	tagline:
+		"Thanks for purchasing BenKon solutions. Here you will find product FAQs, product user manuals and installation videos for your device.",
+	favicon: "img/favicon.png",
 
-  // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+	// Set the production url of your site here
+	url: "https://your-docusaurus-site.example.com",
+	// Set the /<baseUrl>/ pathname under which your site is served
+	// For GitHub pages deployment, it is often '/<projectName>/'
+	baseUrl: "/",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	organizationName: "facebook", // Usually your GitHub org/user name.
+	projectName: "docusaurus", // Usually your repo name.
 
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
+	onBrokenLinks: "throw",
+	onBrokenMarkdownLinks: "warn",
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: "vi",
-    locales: ["vi", "en"],
-  },
+	// Even if you don't use internationalization, you can use this field to set
+	// useful metadata like html lang. For example, if your site is Chinese, you
+	// may want to replace "en" with "zh-Hans".
+	i18n: {
+		defaultLocale: "vi",
+		locales: ["vi", "en"],
+	},
 
-  presets: [
-    [
-      "classic",
-      {
-        docs: {
-          sidebarPath: "./sidebars.ts",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-        },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ["rss", "atom"],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-          // Useful options to enforce blogging best practices
-          onInlineTags: "warn",
-          onInlineAuthors: "warn",
-          onUntruncatedBlogPosts: "warn",
-        },
-        theme: {
-          customCss: "./src/css/custom.css",
-        },
-      } satisfies Preset.Options,
-    ],
-  ],
+	presets: [
+		[
+			"classic",
+			{
+				docs: {
+					sidebarPath: "./sidebars.ts",
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+				},
+				blog: {
+					showReadingTime: true,
+					feedOptions: {
+						type: ["rss", "atom"],
+						xslt: true,
+					},
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+					// Useful options to enforce blogging best practices
+					onInlineTags: "warn",
+					onInlineAuthors: "warn",
+					onUntruncatedBlogPosts: "warn",
+				},
+				theme: {
+					customCss: "./src/css/custom.css",
+				},
+			} satisfies Preset.Options,
+		],
+	],
 
-  themeConfig: {
-    // Replace with your project's social card
-    // image: "img/docusaurus-social-card.jpg",
-    navbar: {
-      title: "BenKon Support",
-      logo: {
-        alt: "BenKon Logo",
-        src: "img/benkon-logo-symbol.svg",
-      },
-      items: [
-        {
-          type: "docSidebar",
-          sidebarId: "tutorialSidebar",
-          position: "left",
-          label: "User Manuals",
-        },
-        { to: "/faq", label: "FAQs", position: "left" },
-        { to: "/changelog", label: "Changelog", position: "left" },
-        {
-          type: "localeDropdown",
-          position: "right",
-        },
-      ],
-    },
-    footer: {
-      style: "dark",
-      links: [],
-      copyright: `Copyright © ${new Date().getFullYear()} BenKon Corp.`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.oneDark,
-    },
-  } satisfies Preset.ThemeConfig,
+	themeConfig: {
+		// Replace with your project's social card
+		// image: "img/docusaurus-social-card.jpg",
+		navbar: {
+			title: "BenKon Support",
+			logo: {
+				alt: "BenKon Logo",
+				src: "img/benkon-logo-symbol.svg",
+			},
+			items: [
+				{
+					type: "docSidebar",
+					sidebarId: "tutorialSidebar",
+					position: "left",
+					label: "User Manuals",
+				},
+				{ to: "/faq", label: "FAQs", position: "left" },
+				{ to: "/changelog", label: "Changelog", position: "left" },
+				{
+					type: "localeDropdown",
+					position: "right",
+				},
+			],
+		},
+		footer: {
+			style: "dark",
+			links: [],
+			copyright: `Copyright © ${new Date().getFullYear()} BenKon Corp.`,
+		},
+		prism: {
+			theme: prismThemes.github,
+			darkTheme: prismThemes.oneDark,
+		},
+	} satisfies Preset.ThemeConfig,
+
+	plugins: [
+		async function myPlugin(context, options) {
+			return {
+				name: "docusaurus-tailwindcss",
+				configurePostCss(postcssOptions) {
+					// Appends TailwindCSS and AutoPrefixer.
+					postcssOptions.plugins.push(require("tailwindcss"));
+					postcssOptions.plugins.push(require("autoprefixer"));
+					return postcssOptions;
+				},
+			};
+		},
+	],
 };
 
 export default config;
